674a0b1934ad40c1dd1a9abb58c2e0a57f38c065
Kim.Yushin <offyushin@naver.com>
2023-12-22 01:18:42 +0900

    유튜브 UI 하단네비게이션바 홈 밖에 안나옴....

--- lib/main_bottom.dart
+++ lib/main_bottom.dart
@@ -6,12 +6,41 @@ class MyBottomNavigationBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(icon: Image.asset('assets/homeicon.png',scale: 20,), label: '홈'),
-          BottomNavigationBarItem(icon: Image.asset('assets/shortsicon.png',scale: 20,), label: 'Shorts'),
-          BottomNavigationBarItem(icon: Image.asset('assets/uploadicon.png',scale: 20,), label: ''),
-          BottomNavigationBarItem(icon: Image.asset('assets/subscriptionsicon.png',scale: 20,), label: '구독'),
-          BottomNavigationBarItem(icon: Image.asset('assets/archiveicon.png',scale: 20,), label: '보관함'),
-        ],);
+      items: [
+        BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/homeicon.png',
+              scale: 20,
+            ),
+            label: '홈'),
+        BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/shortsicon.png',
+              scale: 20,
+            ),
+            label: 'Shorts'),
+        BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/uploadicon.png',
+              scale: 20,
+            ),
+            label: ''),
+        BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/subscriptionsicon.png',
+              scale: 20,
+            ),
+            label: '구독'),
+        BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/archiveicon.png',
+              scale: 20,
+            ),
+            label: '보관함'),
+      ],
+      selectedItemColor: Colors.white,
+      unselectedItemColor: Colors.white,
+      type: BottomNavigationBarType.shifting,
+    );
   }
 }
