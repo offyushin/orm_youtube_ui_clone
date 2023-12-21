d96346ff2d655cc1cb5e66e04d5683bdca649505
Kim.Yushin <offyushin@naver.com>
2023-12-21 23:31:09 +0900

    유튜브 UI 영상 밑 글씨 나오게 하기..

--- lib/main_listview.dart
+++ lib/main_listview.dart
@@ -9,28 +9,57 @@ class MainListView extends StatelessWidget {
       child: ListView(
         scrollDirection: Axis.vertical,
         children: [
-          SizedBox(
+          Column(
+            children: [
+              Container(
+                width: double.infinity,
+                height: 300,
+                child: Image(
+                  image: AssetImage('assets/videoimage1.jpg'),
+                ),
+              ),
+              Row(
+                children: [
+                  Image(
+                    image: AssetImage('assets/profileicon.png'),
+                    width: 50,
+                    height: 50,
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        '플레이리스트리스트',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      Text(
+                        '플레이리 조회수 300만회 10시간 전',
+                        style: TextStyle(fontSize: 10),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Container(
             width: double.infinity,
             height: 300,
-            child: Image(image: AssetImage('assets/videoimage1.jpg'),
+            child: Image(
+              image: AssetImage('assets/videoimage1.jpg'),
             ),
           ),
           Container(
             width: double.infinity,
             height: 300,
-            child: Image(image: AssetImage('assets/videoimage1.jpg'),
+            child: Image(
+              image: AssetImage('assets/videoimage1.jpg'),
             ),
           ),
           Container(
             width: double.infinity,
             height: 300,
-            child: Image(image: AssetImage('assets/videoimage1.jpg'),
-            ),
-          ),
-          Container(
-            width: double.infinity,
-            height: 300,
-            child: Image(image: AssetImage('assets/videoimage1.jpg'),
+            child: Image(
+              image: AssetImage('assets/videoimage1.jpg'),
             ),
           ),
         ],
