5f33ab9fc8a9bab35482306ef0ec8a7ac517300a
Kim.Yushin <offyushin@naver.com>
2023-12-22 00:28:03 +0900

    유튜브 UI 영상-타이틀 간격 전체적으로 코드 수정

--- lib/main_listview.dart
+++ lib/main_listview.dart
@@ -5,65 +5,232 @@ class MainListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: ListView(
-        scrollDirection: Axis.vertical,
-        children: [
-          Column(
-            children: [
-              Container(
-                width: double.infinity,
-                height: 300,
-                child: Image(
-                  image: AssetImage('assets/videoimage1.jpg'),
-                ),
-              ),
-              Row(
-                children: [
-                  Image(
-                    image: AssetImage('assets/profileicon.png'),
-                    width: 50,
-                    height: 50,
-                  ),
-                  Column(
-                    children: [
-                      Text(
-                        '플레이리스트리스트',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      Text(
-                        '플레이리 조회수 300만회 10시간 전',
-                        style: TextStyle(fontSize: 10),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ],
+    return ListView(
+      children: [
+        Image(
+          image: AssetImage('assets/videoimage1.jpg'),
+        ),
+        Container(
+          height: 1,
+          color: Colors.white,
+        ),
+        ListTile(
+          title: Text(
+            '플리플플플레이리스트',
+            style: TextStyle(fontWeight: FontWeight.bold),
           ),
-          Container(
-            width: double.infinity,
-            height: 300,
-            child: Image(
-              image: AssetImage('assets/videoimage1.jpg'),
-            ),
+          subtitle: Text('플레이리스트 조회수 300만회 10시간 전'),
+        ),
+        Image(
+          image: AssetImage('assets/videoimage2.jpg'),
+        ),
+        Container(
+          height: 1,
+          color: Colors.white,
+        ),
+        ListTile(
+          title: Text(
+            '플리플플플레이리스트',
+            style: TextStyle(fontWeight: FontWeight.bold),
           ),
-          Container(
-            width: double.infinity,
-            height: 300,
-            child: Image(
-              image: AssetImage('assets/videoimage1.jpg'),
-            ),
+          subtitle: Text('플레이리스트 조회수 300만회 10시간 전'),
+        ),
+        Image(
+          image: AssetImage('assets/videoimage3.jpg'),
+        ),
+        Container(
+          height: 1,
+          color: Colors.white,
+        ),
+        ListTile(
+          title: Text(
+            '플리플플플레이리스트',
+            style: TextStyle(fontWeight: FontWeight.bold),
           ),
-          Container(
-            width: double.infinity,
-            height: 300,
-            child: Image(
-              image: AssetImage('assets/videoimage1.jpg'),
-            ),
+          subtitle: Text('플레이리스트 조회수 300만회 10시간 전'),
+        ),
+        Image(
+          image: AssetImage('assets/videoimage4.jpg'),
+        ),
+        Container(
+          height: 1,
+          color: Colors.white,
+        ),
+        ListTile(
+          title: Text(
+            '플리플플플레이리스트',
+            style: TextStyle(fontWeight: FontWeight.bold),
           ),
-        ],
-      ),
+          subtitle: Text('플레이리스트 조회수 300만회 10시간 전'),
+        ),
+        Image(
+          image: AssetImage('assets/videoimage5.jpg'),
+        ),
+        Container(
+          height: 1,
+          color: Colors.white,
+        ),
+        ListTile(
+          title: Text(
+            '플리플플플레이리스트',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          subtitle: Text('플레이리스트 조회수 300만회 10시간 전'),
+        ),
+        Image(
+          image: AssetImage('assets/videoimage6.jpg'),
+        ),
+        Container(
+          height: 1,
+          color: Colors.white,
+        ),
+        ListTile(
+          title: Text(
+            '플리플플플레이리스트',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          subtitle: Text('플레이리스트 조회수 300만회 10시간 전'),
+        ),
+        Image(
+          image: AssetImage('assets/videoimage7.jpg'),
+        ),
+        Container(
+          height: 1,
+          color: Colors.white,
+        ),
+        ListTile(
+          title: Text(
+            '플리플플플레이리스트',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          subtitle: Text('플레이리스트 조회수 300만회 10시간 전'),
+        ),
+        Image(
+          image: AssetImage('assets/videoimage8.jpg'),
+        ),
+        Container(
+          height: 1,
+          color: Colors.white,
+        ),
+        ListTile(
+          title: Text(
+            '플리플플플레이리스트',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          subtitle: Text('플레이리스트 조회수 300만회 10시간 전'),
+        ),
+        Image(
+          image: AssetImage('assets/videoimage9.jpg'),
+        ),
+        Container(
+          height: 1,
+          color: Colors.white,
+        ),
+        ListTile(
+          title: Text(
+            '플리플플플레이리스트',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          subtitle: Text('플레이리스트 조회수 300만회 10시간 전'),
+        ),
+        Image(
+          image: AssetImage('assets/videoimage10.jpg'),
+        ),
+        Container(
+          height: 1,
+          color: Colors.white,
+        ),
+        ListTile(
+          title: Text(
+            '플리플플플레이리스트',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          subtitle: Text('플레이리스트 조회수 300만회 10시간 전'),
+        ),
+      ],
     );
   }
 }
+
+// @override
+// Widget build(BuildContext context) {
+//   return Expanded(
+//     child: ListView(
+//       scrollDirection: Axis.vertical,
+//       children: [
+//         Column(
+//           children: [
+//             Container(
+//               width: double.infinity,
+//               height: 300,
+//               child: const Image(
+//                 image: AssetImage('assets/videoimage1.jpg'),
+//               ),
+//             ),
+//             const Row(
+//               children: [
+//                 Image(
+//                   image: AssetImage('assets/profileicon.png'),
+//                   width: 50,
+//                   height: 50,
+//                 ),
+//                 Column(
+//                   children: [
+//                     Text(
+//                       '플레이리스트리스트',
+//                       style: TextStyle(fontSize: 20),
+//                     ),
+//                     Text(
+//                       '플레이리 조회수 300만회 10시간 전',
+//                       style: TextStyle(fontSize: 10),
+//                     ),
+//                   ],
+//                 ),
+//               ],
+//             ),
+//           ],
+//         ),
+//         Column(
+//           children: [
+//             Container(
+//               width: double.infinity,
+//               height: 300,
+//               child: const Image(
+//                 image: AssetImage('assets/videoimage1.jpg'),
+//               ),
+//             ),
+//             const Row(
+//               children: [
+//                 Image(
+//                   image: AssetImage('assets/profileicon.png'),
+//                   width: 50,
+//                   height: 50,
+//                 ),
+//                 Column(
+//                   children: [
+//                     Text(
+//                       '플레이리스트리스트',
+//                       style: TextStyle(fontSize: 20),
+//                     ),
+//                     Text(
+//                       '플레이리 조회수 300만회 10시간 전',
+//                       style: TextStyle(fontSize: 10),
+//                     ),
+//                   ],
+//                 ),
+//               ],
+//             ),
+//           ],
+//         ),
+//         Container(
+//           width: double.infinity,
+//           height: 300,
+//           child: const Image(
+//             image: AssetImage('assets/videoimage1.jpg'),
+//           ),
+//         ),
+//       ],
+//     ),
+//   );
+// }
+// }
